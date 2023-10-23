467594ff987e4f5b0c6d824b70ca933eec85efed
Sam Aven <samjaven33@gmail.com>
2023-10-22 23:41:48 -0500

    Flushed out functions and finished
    Flushed out the rest of the program and added "using namespace std" also looked to gpt to help with file input since it was broken previously. Once this was done flushed out the modification functions for the matricies

--- Assignment1/main.cpp
+++ Assignment1/main.cpp
@@ -9,62 +9,220 @@ Creation Date: 10/18/2023
 
 #include <iostream>
 #include <fstream>
-#include <string>
+#include <cstring>
 #include <vector>
+#include <sstream>
+using namespace std;
 
-//Function to read and resize a matrix from a file
-std:: vector<std:: vector<int>> readMatrix(std:: ifstream& file){
-    int rows, cols;
+//Struct that stores data about the matrices read from the given file
+struct Data
+{
+    int size;
+    int **matrix_1;
+    int **matrix_2;
+};
 
-    //read the size of the matrix
-    if (!(file >> rows)){
-        std:: cerr << "Failed to retrieve matrix size" << std::endl; //if size is unable to be determined print error message
-        exit(1);
+// Function Prototypes (I tried to put the struct here as well, but it is required for where it is.)
+Data matrixLoad(string filename);
+void matrixPrint(int **matrix, int size);
+int **matrixAdd(Data data);
+int **matrixSubtract(Data data);
+int **matrixProduct(Data data);
+
+int main() {
+    cout << "Sam Aven" << endl;
+    cout << "Lab #6: Matrix Manipulation" << endl;
+
+    // Load matrices from file
+    Data data = matrixLoad("matrix_input.txt");
+    int size = data.size;
+    int** matrix_1 = data.matrix_1;
+    int** matrix_2 = data.matrix_2;
+    cout << "Matrix Sizes: " << size << " x " << size << endl;
+
+    cout << "\nMatrix A" << endl;
+    matrixPrint(matrix_1, size);
+    cout << "Matrix B" << endl;
+    matrixPrint(matrix_2, size);
+
+    cout << "Matrix Sum (A + B)" << endl;
+    int **addedMatrix = matrixAdd(data);
+
+    cout << "Matrix Product (A * B)" << endl;
+    int **productMatrix = matrixProduct(data);
+
+    cout << "Matrix Difference (A - B)" << endl;
+    int **subtractedMatrix = matrixSubtract(data);
+
+    // Return from main
+    return 0;
+}
+
+// Loads 2 matrices in from a text file and returns an array containing the size and both matrices
+Data matrixLoad(string filename) {
+    // Initialize Variables
+    ifstream file;
+    string text;
+    int size;
+    static const int maxSize = 100;
+    int line = 1;
+    
+    // Open file
+    file.open(filename);
+
+    // Get the first line to get the size
+    getline(file, text);
+    size = atoi(text.c_str());
+
+    // Check against max size
+    if (size > maxSize)
+    {
+        printf("ERROR: Size of square matrix must be less than %i!", maxSize);
+        terminate();
     }
-    cols = rows; //Since the input file only contains one value to determine the dimensions we can assume they are the same
 
-    //Create a vector of vectors with the specified size
-    std::vector<std::vector<int>> matrix(rows, std::vector<int>(cols));
+    // Allocate memory for matrices
+    int **matrix_1 = new int*[size];
+    int **matrix_2 = new int*[size];
+    for (int i = 0; i < size; ++i) {
+        matrix_1[i] = new int[size]();
+        matrix_2[i] = new int[size]();
+    }
+    
+    while (getline(file, text)) {
+        istringstream iss(text);
+        int col = 0;
+        int value;
 
-    //Read and populate the matrix
-    for (int i = 0; i < rows; i++) {
-        for (int j = 0; j < cols; j++) {
-            if (!(file >> matrix[i][j])) {
-                std::cerr << "Failed to read matrix data." << std::endl;
-                exit(1);
+        // Reading the first matrix
+        if (1 <= line && line <= size) {
+            while (iss >> value) {
+                matrix_1[line-1][col++] = value;
+            }
+        }
+
+        // Reading the second matrix
+        if ((size + 1) <= line && line <= (size * 2)) {
+            while (iss >> value) {
+                matrix_2[line-(size+1)][col++] = value;
+            }
+        }
+
+        line++;
+    }
+
+    // Close file
+    file.close();
+
+    // Return the matrices
+    Data result {size, matrix_1, matrix_2};
+    return result;
+}
+
+// Cleanly prints a given matrix
+void matrixPrint(int **matrix, int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        for (int j = 0; j < size; j++)
+        {
+            printf("%5i", matrix[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+// Adds 2 matrices together and returns a single matrix 
+int **matrixAdd(Data data)
+{
+    // Initialize variables
+    int size = data.size;
+    int **matrix_1 = data.matrix_1;
+    int **matrix_2 = data.matrix_2;
+
+    // Allocate memory for resulting
+    int **result = new int*[size];
+    for (int i = 0; i < size; ++i) {
+        result[i] = new int[size]();
+    }
+
+    // Sum every value in the matrix
+    for (int i = 0; i < size; i++)
+    {
+        for (int j = 0; j < size; j++)
+        {
+            int value = matrix_1[i][j] + matrix_2[i][j];
+            result[i][j] = value;
+        }
+    }
+
+    // Print the matrix
+    matrixPrint(result, size);
+
+    // Return the matrix
+    return result;
+}
+
+// Subtracts 2 matrices and returns a single matrix
+int **matrixSubtract(Data data)
+{
+    // Initialize variables
+    int size = data.size;
+    int **matrix_1 = data.matrix_1;
+    int **matrix_2 = data.matrix_2;
+
+    // Allocate memory for resulting
+    int **result = new int*[size];
+    for (int i = 0; i < size; ++i) {
+        result[i] = new int[size]();
+    }
+
+    // Subtract every value in the matrix
+    for (int i = 0; i < size; i++)
+    {
+        for (int j = 0; j < size; j++)
+        {
+            int value = matrix_1[i][j] - matrix_2[i][j];
+            result[i][j] = value;
+        }
+    }
+
+    // Print the matrix
+    matrixPrint(result, size);
+
+    // Return the matrix
+    return result;
+}
+
+// Multiplies 2 matrices together and returns a single matrix
+int **matrixProduct(Data data)
+{
+    // Initialize variables
+    int size = data.size;
+    int **matrix_1 = data.matrix_1;
+    int **matrix_2 = data.matrix_2;
+
+    // Allocate memory for resulting
+    int **result = new int*[size];
+    for (int i = 0; i < size; ++i) {
+        result[i] = new int[size]();
+    }
+
+    // Perform matrix multiplication
+    for (int i = 0; i < size; i++)
+    {
+        for (int j = 0; j < size; j++)
+        {
+            for (int k = 0; k < size; k++)
+            {
+                result[i][j] += matrix_1[i][k] * matrix_2[k][j];
             }
         }
     }
 
-    return matrix;
-}
+    // Print the matrix
+    matrixPrint(result, size);
 
-int main(){
-    std::ifstream file("matrix_input.txt"); //opens input file
-    
-    if (!file.is_open()){ //checks if we fail to open the input file
-        std:: cerr << "Input file failed to open" << std:: endl;
-        return 1;
-    }
-
-    //Read and store the first matrix
-    std::vector<std::vector<int>> matrix1 = readMatrix(file);
-
-    //Read and store the second matrix
-    std::vector<std::vector<int>> matrix2 = readMatrix(file);
-
-    //Close file
-    file.close();
-
-    // Printing the matrices
-    std::cout << "Matrix 1:" << std::endl;
-    for (const auto& row : matrix1) {
-        for (int value : row) {
-            std::cout << value << ' ';
-        }
-        std::cout << std::endl;
-    }
-    
-
-    return 0;
+    // Return the matrix
+    return result;
 }
